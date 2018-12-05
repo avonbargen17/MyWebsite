c89592264dd6a23b77d2413926b2f6a309b6872e
Unknown <alexander.vonbargen.2021@student.lhschools.org>
2018-12-05 12:29:49 -0500

    New Javascript page

---
+++ javascript.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+  	document.getElementById("h").style.display = "none";
+	document.getElementById("p1").style.display = "block";
+	document.getElementById("button1").style.display = "none";
+	}
